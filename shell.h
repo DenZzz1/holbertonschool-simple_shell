fefa3db5cdaccb04a74c30e68c32a6cc3d21f08a
DenZzz1 <m2nky2fficiel@gmail.com>
2026-01-04 10:09:51 +0000

    add shell.h

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT 1024
+
+extern char **environ;
+
+void print_prompt(void);
+ssize_t read_input(char *buffer, size_t size);
+char *parse_input(char *input);
+void execute_command(char *command);
+
+#endif
+
